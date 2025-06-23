113602db25541436104963804360dd48755cb64f
Ashish Kumar <rajashishku.2014@gmail.com>
2025-06-24 00:04:14 +0530

    DRAGON FIRE: First Windows Sacrifice

---
+++ fire.py
@@ -0,0 +1,6 @@
+def dragon_attack():
+    print("ashish"*100)
+    return "code fire"
+
+if __name__ == "__main__":
+	dragon_attack()  
---
+++ flame_thrower.py
@@ -0,0 +1,4 @@
+count =2
+while count >=1:
+	print("BURN")
+	count +=1
